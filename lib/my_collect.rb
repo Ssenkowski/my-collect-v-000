f5be5b8ce5e381d101a71d5e171dfff97c7bc2ea
Steven Senkowski <scsenkowski@gmail.com>
2017-11-10 00:55:49 +0000

    automatically backed up by learn

--- lib/my_collect.rb
+++ lib/my_collect.rb
@@ -1,7 +1,8 @@
 def my_collect(student_course)
     i = 0
+    collection = []
     while i < student_course.length
-      yield (student_course[i])
+      collection << yield (student_course[i])
       i += 1
     end
 end
